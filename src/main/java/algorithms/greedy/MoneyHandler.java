f3b709a264cc3a19722d00ffee2529b7cd34714d
KongCPH <kong@ek.dk>
2026-04-23 10:57:22 +0200

    minor

--- src/main/java/algorithms/greedy/MoneyHandler.java
+++ src/main/java/algorithms/greedy/MoneyHandler.java
@@ -3,19 +3,22 @@ package algorithms.greedy;
 public class MoneyHandler {
 
     public static void main(String[] args) {
+
         // standard bills
-        System.out.println("Udbetaling med 50, 100, 500 og 1000 kr-sedler:");
+        System.out.println("Udbetaling med 50, 100, 500, 1000, 2000 og 5000 kr-sedler:");
         int[] bills = {5000, 2000, 1000, 500, 100, 50};
-        greedyChange(bills, 1200);
-        greedyChange(bills, 1300);
-        greedyChange(bills, 1800);
+        greedyChange(bills, 550);
+        greedyChange(bills, 3700);
+        greedyChange(bills, 750);
+        greedyChange(bills, 2250);
+        greedyChange(bills, 150);
 
         // 600 kr introduceres
-        System.out.println("600 kr-seddel introduceres:");
+        /*System.out.println("600 kr-seddel introduceres:");
         int[] moreBills = {5000, 2000, 1000, 600, 500, 100, 50};
         greedyChange(moreBills, 1200);
         greedyChange(moreBills, 1300);
-        greedyChange(moreBills, 1800);
+        greedyChange(moreBills, 1800);*/
 
     }
 
